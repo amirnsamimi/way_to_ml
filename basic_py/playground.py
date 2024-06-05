3389027c76bfa64d189706d6be4b01e7c58e40f4
amirnsamimi <amirnsamimi@gmail.com>
2024-06-05 15:29:02 +0330

    creating requirments.txt

--- basic_py/playground.py
+++ basic_py/playground.py
@@ -305,3 +305,5 @@ pd_groupby_single_variable = pd_df_from_CSV.groupby("director")["duration"].agg(
 pd_pivotTable = pd_df_from_CSV.pivot_table(values="duration",index="director", aggfunc=["sum","max"]) #same as group by but as default it summarize using mean but can pass an alternative to aggfunc
 
 
+
+
